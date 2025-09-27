d7593dbd77e67809c308bc2b138e25f291e20a9a
SupernerdX <66530288+SupernerdX@users.noreply.github.com>
2025-09-27 16:14:39 -0500

    merging mangers

--- UT 2025 Game Jam/Assets/Scripts/Gameflow/MiniGameManager.cs
+++ UT 2025 Game Jam/Assets/Scripts/Gameflow/MiniGameManager.cs
@@ -6,6 +6,7 @@ public class MiniGameManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public List<MiniGameData> miniGames = new List<MiniGameData>();
+    [SerializeField] private int selectedGameIndex;
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
@@ -25,15 +26,22 @@ public class MiniGameManager : MonoBehaviour
 
     public string randomizeMiniGames()
     {
-        int randomIndex = Random.Range(0, miniGames.Count);
-        Debug.Log(miniGames[randomIndex].miniGameName);
-        return miniGames[randomIndex].miniGameName;
+        selectedGameIndex = Random.Range(0, miniGames.Count);
+        Debug.Log(miniGames[selectedGameIndex].miniGameName);
+        return miniGames[selectedGameIndex].miniGameName;
 
+    }
+
+    public float timeToCompleteGame()
+    {
+        Debug.Log(miniGames[selectedGameIndex].timeLimit);
+        return miniGames[selectedGameIndex].timeLimit;
     }
     
     void LoadMiniGames()
     {
         var miniGameName = randomizeMiniGames();
+        var mingameTime = timeToCompleteGame();
         SceneManager.LoadScene(miniGameName);
     }
 
