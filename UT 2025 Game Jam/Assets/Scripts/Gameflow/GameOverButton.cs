b686578db6813dc7c1e2b8c9abb17c1141a2c053
anderstaylor <111925828+AndersTaylor@users.noreply.github.com>
2025-09-28 14:46:41 -0500

    game over

---
+++ UT 2025 Game Jam/Assets/Scripts/Gameflow/GameOverButton.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButton : MonoBehaviour
+{
+    public void Awake()
+    {
+        Destroy(FindFirstObjectByType<GameManager>().gameObject);
+        Destroy(FindFirstObjectByType<AudioManager>().gameObject);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
