bc3170d0ee2f2f5f1235bffc626c472581430d27
THEDRIFTER823 <143732372+THEDRIFTER823@users.noreply.github.com>
2025-09-28 11:31:07 -0500

    Reapply "Added Event Bus system to game manger system"
    This reverts commit c27cdd030372eed7c5bb8540b73037bd2e6205c5.

---
+++ UT 2025 Game Jam/Assets/Scripts/Games/RepairNode/RepairNode.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class RepairNode : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite onSprite;
+    public bool repaired;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        repaired = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // changes color once "fixed"
+        spriteRenderer.sprite = onSprite;
+        repaired = true;
+    }
+}
