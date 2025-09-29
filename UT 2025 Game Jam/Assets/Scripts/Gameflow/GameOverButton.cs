1398f494fcf25b8335131369eec5aa5ba803c67d
anderstaylor <111925828+AndersTaylor@users.noreply.github.com>
2025-09-28 21:17:05 -0500

    fix

--- UT 2025 Game Jam/Assets/Scripts/Gameflow/GameOverButton.cs
+++ UT 2025 Game Jam/Assets/Scripts/Gameflow/GameOverButton.cs
@@ -1,13 +1,22 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverButton : MonoBehaviour
 {
+    public TMP_Text scoreText;
+    private int score;
+    
     public void Awake()
     {
-       Destroy(FindFirstObjectByType<GameManager>().gameObject);
-       Destroy(FindFirstObjectByType<AudioManager>().gameObject);
+        scoreText = GameObject.FindGameObjectWithTag("ScoreText").GetComponent<TMP_Text>();
+        
+        score = ScoreManager.Instance.totalScore;
+        scoreText.text = "Score: " + score;
+        
+        Destroy(FindFirstObjectByType<GameManager>().gameObject);
+        Destroy(FindFirstObjectByType<AudioManager>().gameObject);
     }
 
     public void LoadGame()
